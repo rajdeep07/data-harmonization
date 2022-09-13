bb165f9e8a08917d59552a9ec6ea08306344c9aa
NavazdeenS <navazdeen.shamsu@tigeranalytics.com>
2022-09-13 10:59:34 +0530

    enhance sanitizer

--- data_harmonization/main/code/tiger/Sanitizer.py
+++ data_harmonization/main/code/tiger/Sanitizer.py
@@ -1,9 +1,10 @@
-from data_harmonization.main.code.com.tiger.data.transformer.utils import StringSupport
-from data_harmonization.main.code.com.tiger.data.transformer import CityTransformer, NameTransformer, \
-    PostalAddressTransformer, StateTransformer, ZipTransformer, IntegerTypeTransformer, StringTypeTransformer
-from data_harmonization.main.code.com.tiger.data.model import GeocodedAddress, PostalAddress
-from data_harmonization.main.code.com.tiger.data.model.dataclass import RawEntity, CleansedRawEntity
-from data_harmonization.main.code.com.tiger.data.model.datamodel import *
+# from data_harmonization.main.code.tiger.transformer.utils import StringSupport
+# from data_harmonization.main.code.tiger.transformer import CityTransformer, NameTransformer, \
+#     PostalAddressTransformer, StateTransformer, ZipTransformer, IntegerTypeTransformer, StringTypeTransformer
+# from data_harmonization.main.code.tiger.model import GeocodedAddress, PostalAddress
+# from data_harmonization.main.code.tiger.model.dataclass import RawEntity, CleansedRawEntity
+from dataclasses import dataclass
+from data_harmonization.main.code.tiger.model.datamodel import *
 
 
 class Sanitizer():
@@ -11,13 +12,14 @@ class Sanitizer():
     def _get_attr_list(self, should_print=False):
         items = RawEntity.__dict__.items()
         if should_print:
-            [print(f"attribute: {k}  value: {v}") for k, v in items]
-        return items
+            [print(f"attribute: {k}  value: {v}") for k, v in items if k in ("__annotations__")]
+        return(value for item, value in items if item in ("__annotations__"))
 
     def toRawEntity(self, cls):
 
-        raw_attribute_lists = self._get_attr_list(RawEntity)
-        cls_map = {key:globals[value.replace("Optional[", "").replace("]","")] for key , value in raw_attribute_lists} #{id:"int", "name":"Name"}
+        raw_attribute_lists = list(self._get_attr_list())[0]
+        print(raw_attribute_lists, globals())
+        cls_map = {key:globals().get([value.replace("Optional[", "").replace("]", "")]) for key , value in raw_attribute_lists.items()}
 
         raw_kw = {}
         for raw_attribute, type in raw_attribute_lists.items():
@@ -32,6 +34,7 @@ class Sanitizer():
                     raw_kw[raw_attribute] = cls.__getattribute__(raw_attribute)
 
         raw_entity_object = RawEntity(**raw_kw)
+        print(raw_entity_object)
         return raw_entity_object
     
     def _apply_transformer(self):
@@ -61,10 +64,22 @@ class Sanitizer():
     @classmethod
     def __getattribute__(cls, attr):
         try:
-            return {k: _apply_transformer(v) in cls.__dict__[attr].items()}
+            return {k: cls._apply_transformer(v) for k, v in cls.__dict__[attr].items()}
         except AttributeError:
             raise AttributeError(attr)
 
     def _get_sanitized_entity(self):
-        for attr, val in __getattribute__().items():
+        for attr, val in self.__getattribute__().items():
             setattr(self, CleansedRawEntity, attr, val)
+
+if __name__ == "__main__":
+    @dataclass
+    class TestClass:
+        name="name"
+        id=1
+        city = "new_city"
+        zipcode = 123456
+        address = "123, A st, new_city"
+    snt = Sanitizer()
+    snt.toRawEntity(TestClass)
+
