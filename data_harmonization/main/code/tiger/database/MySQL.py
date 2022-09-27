0b939042e597dc9f0d1a36c74b096a5b1b04a44a
rajdeep <rajdeep@revinate.com>
2022-09-27 14:19:42 -0700

    add utility code to add spark + database connections

---
+++ data_harmonization/main/code/tiger/database/MySQL.py
@@ -0,0 +1,32 @@
+import data_harmonization.main.resources.config as config
+import mysql.connector
+import sqlalchemy
+
+
+class MySQL:
+    def __init__(self, host, database, user, password):
+        self.database = database
+        self.host = host
+        self.user = user
+        self.password = password
+        self.mydb = mysql.connector.connect(
+            host = self.host,
+            user = self.user,
+            password = self.password,
+            database = self.database
+        )
+        self.url = f'mysql+mysqlconnector://{self.user}:{self.password}@{self.host}/{self.database}'
+        self.engine = sqlalchemy.create_engine(self.url)
+        self.mycursor = self.mydb.cursor()
+
+    def get_result(self, to_print=False, title=''):
+        result = self.mycursor.fetchall()
+        if to_print:
+            print('\n')
+            if title:
+                print(f'{title}')
+
+            for x in result:
+                print(x)
+
+        return result
--- data_harmonization/main/resources/config.py
+++ data_harmonization/main/resources/config.py
@@ -1,12 +1,18 @@
 from os import environ as env
+import netifaces
 
 # cloud credentials
 awsAccessKey = "AWS_ACCESS_KEY_ID"
 awsSecretKey = "AWS_SECRET_ACCESS_KEY"
 awsDefaultRegion = "AWS_DEFAULT_REGION"
 
-# database credentials
 
+# database credentials
+mysqlUser = "default"
+mysqlPassword = "default"
+mysqlLocalHost = netifaces.gateways()['default'][2][0]
+mysqlDriver = "com.mysql.cj.jdbc.Driver"
+mysqlPort = "3306"
 
 
 # logging credentials
