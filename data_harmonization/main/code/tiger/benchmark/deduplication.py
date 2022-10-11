729b6a24f0574eb3dc269da16e17849f0bec07a8
Saikat Saha <saikat.saha@tigeranalytics.com>
2022-10-11 23:22:01 +0530

    deduplication and deep_learning classifier

--- data_harmonization/main/code/tiger/benchmark/deduplication.py
+++ data_harmonization/main/code/tiger/benchmark/deduplication.py
@@ -1,80 +1,103 @@
 import os
-import sys
-from os import listdir
 
-import numpy as np
 import pandas as pd
 import pandas_dedupe
-import xlrd
+
+from data_harmonization.main.code.tiger.spark.SparkClass import SparkClass
 
 
 class Deduplication:
 
+    def __init__(self):
+        self.raw_entity_table_name = "Raw_Entity"
+
+    def get_data(self, table: str = "Raw_Entity", max_length: int=20000) -> pd.DataFrame:
+        spark = SparkClass()
+        df = spark.read_from_database_to_dataframe(table)
+        pandas_df = df.toPandas()
+        # if there are more than max_length records, take randomly sampled 20000 records
+        if len(pandas_df.index) > max_length:
+            pandas_df = pandas_df.sample(n=max_length)
+        return pandas_df
+
     # This method is used for model training.
-    def model_training(self):
-
-        # Generate dataframe
-        current_dir = os.path.dirname(os.path.realpath(__file__))
-        target_dir = os.path.sep.join(current_dir.split(os.path.sep)[:-3])
-        filenames = listdir(target_dir + "/data/")
-        csv_filenames = [
-            filename
-            for filename in filenames
-            if filename.endswith(".csv") and not filename.startswith("benchmark")
-        ]
-
-        # Read from MYSQL + RawEntity ==> PySpark DF
-        # PySparkDf to toPandas()
-        # len(PySparkDf) < 20,000 Records :: Take the entire dataset
-        # If not, randomly sample 20k records.
-        # if not None, features_for_deduplication [User Provided] Subset pandas dataframe
-        # if None, use all columns
-
-        dataframe = pd.DataFrame()  # correct data structure here ?
-        for csv_file in csv_filenames:
-            dataframe = dataframe.append(pd.read_csv(target_dir + f"/data/{csv_file}"))
-
-        # Run Dedupe Model
-        # DataFrame :: features_for_deduplication + id columns
-        dataframe = dataframe[["Name", "Address", "City", "State", "Zip"]]  # "source"
+    def _run_model(self, df: pd.DataFrame, col_names: list = []):
+        df_for_dedupe_model = df.copy()
+        if col_names and len(col_names) > 0:
+            df_for_dedupe_model = df_for_dedupe_model[col_names]
+        else:
+            col_names = list(df_for_dedupe_model.columns)
+        if "id" in col_names:
+                col_names.remove("id")
+        print(col_names)
         final_model = pandas_dedupe.dedupe_dataframe(
-            # features_for_deduplication
-            dataframe, ["Name", "Address", "City", "State", "Zip"]
+            df_for_dedupe_model,
+            col_names,
         )
+
 
         # Cleansing
         final_model = final_model.rename(columns={"cluster id": "cluster_id"})
         final_model.sort_values(
             by=["cluster_id", "confidence"], ascending=True, inplace=True
         )
-
-        # Output the data as csv
-        print("We are writing dataset on")
         # Persist this in MYSQL + benckmark
-        final_model.to_csv(target_dir + "/data/benchmark.csv", mode="w+")
-
-
-        # Few Statistics
-        # Total_Records
-        # Duplicates
-        # %_duplicate = Duplicates/ Total_Records
-
-        # Show 1 cluster which has maximum duplicates
-
+        self._save_data_in_db(final_model, "benchmark")
+        print(self._get_statistics(df_for_dedupe_model, final_model))
 
         return
 
+    def _save_data_in_db(self, df: pd.DataFrame, table: str):
+        spark = SparkClass()
+        spark_df = spark.get_sparkSession().createDataFrame(df)
+        spark.write_to_database_from_df(table, spark_df, mode="overwrite")
+
+    def _get_statistics(self, input_data: pd.DataFrame, model_output: pd.DataFrame):
+        total_records = len(input_data.index)
+        duplicates = len(model_output)
+        number_of_clusters = model_output["cluster_id"].nunique()
+        duplicates = model_output.loc[model_output["confidence"] > 0.8].groupby(by="cluster_id")["confidence"].count().sum()
+        cluster_with_max_duplicates = model_output.loc[model_output["confidence"] > 0.8].groupby(by="cluster_id")["confidence"].count().idxmax()
+        result = {
+            "Total records": total_records,
+            "Number of duplicate sets": number_of_clusters,
+            "Duplicates": duplicates,
+            "% duplicates": duplicates / total_records,
+            "Cluster with maximum duplicates" : cluster_with_max_duplicates
+        }
+        return result
+
+    def train(self, col_names: list = [], df=None):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        target_dir = os.path.sep.join(current_dir.split(os.path.sep)[:-2])
+        if os.path.isfile(target_dir + "/tiger/benchmark/dedupe_dataframe_learned_settings"):
+            os.remove(target_dir + "/tiger/benchmark/dedupe_dataframe_learned_settings")
+        if os.path.isfile(target_dir + "/tiger/benchmark/dedupe_dataframe_training.json"):
+            os.remove(target_dir + "/tiger/benchmark/dedupe_dataframe_training.json")
+        print("removed")
+        if not df:
+            df = self.get_data(self.raw_entity_table_name)
+        return self._run_model(df, col_names)
+
+    def predict(self, col_names: list = [], df=None):
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        target_dir = os.path.sep.join(current_dir.split(os.path.sep)[:-2])
+        if not df:
+            df = self.get_data(self.raw_entity_table_name)
+        if not os.path.isfile(target_dir + "/tiger/benchmark/dedupe_dataframe_learned_settings"):
+            print("Cannot find dedupe_dataframe_learned_settings file")
+        if not os.path.isfile(target_dir + "/tiger/benchmark/dedupe_dataframe_training.json"):
+            print("Cannot find dedupe_dataframe_training.json file")
+        return self._run_model(df, col_names)
 
 if __name__ == "__main__":
-
-
-    # For Training
-    # rm dedupe_dataframe_training.json
-    # rm dedupe_dataframe_learned_settings
-
     dedupe = Deduplication()
     print("Begin Active Learning.")
-    dedupe.model_training()
+    # df = dedupe.get_data("Raw_Entity")
+    # dedupe.train(df)
     print("We are done with training.")
 
     # For Prediction
+    dedupe.predict(['Name', 'Address', 'Zip', 'City', 'id', 'State'])
+    dedupe.predict()
+    print("We are done with prediction.")
