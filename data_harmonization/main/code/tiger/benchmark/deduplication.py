6b928eb547eaafa838268c10652285ddf6dec26e
NavazdeenS <navazdeen1999@gmail.com>
2022-10-14 15:37:56 +0530

    fixed broken updates in ingester module

--- data_harmonization/main/code/tiger/benchmark/deduplication.py
+++ data_harmonization/main/code/tiger/benchmark/deduplication.py
@@ -12,7 +12,7 @@ class Deduplication:
     def __init__(self):
         self.raw_entity_table_name = "rawentity"
 
-    def get_data(self, table: str = "rawentity", max_length: int=20000) -> pd.DataFrame:
+    def get_data(self, table: str = "rawentity", max_length: int = 20000) -> pd.DataFrame:
         spark = SparkClass()
         df = spark.read_from_database_to_dataframe(table)
         pandas_df = df.toPandas()
@@ -29,25 +29,28 @@ class Deduplication:
         else:
             col_names = list(df_for_dedupe_model.columns)
         if "id" in col_names:
-                col_names.remove("id")
+            col_names.remove("id")
         print(col_names)
         final_model = pandas_dedupe.dedupe_dataframe(
             df_for_dedupe_model,
             col_names,
             threshold=0.7,
-            canonicalize=True
+            canonicalize=True,
+
         )
 
-        final_model = final_model[final_model["id"] != final_model["canonical_id"]]
+        final_model = final_model[final_model["id"]
+                                  != final_model["canonical_id"]]
 
         # Cleansing
         final_model = final_model.rename(columns={"cluster id": "cluster_id"})
         print(final_model.columns)
-        final_model.sort_values(
-            by=["cluster_id", "confidence"], ascending=True, inplace=True
-        )
+        # final_model.sort_values(
+        #     by=["cluster_id", "confidence"], ascending=True, inplace=True
+        # )
         # Persist this in MYSQL + benckmark
-        self._save_data_in_db(final_model[['id', 'canonical_id', "cluster_id", "confidence"]], "benchmark")
+        self._save_data_in_db(
+            final_model[['id', 'canonical_id', "cluster_id", "confidence"]], "benchmark")
         print(self._get_statistics(df_for_dedupe_model, final_model))
 
         return
@@ -61,14 +64,16 @@ class Deduplication:
         total_records = len(input_data.index)
         duplicates = len(model_output)
         number_of_clusters = model_output["cluster_id"].nunique()
-        duplicates = model_output.loc[model_output["confidence"] > 0.7].groupby(by="cluster_id")["confidence"].count().sum()
-        cluster_with_max_duplicates = model_output.loc[model_output["confidence"] > 0.7].groupby(by="cluster_id")["confidence"].count().idxmax()
+        duplicates = model_output.loc[model_output["confidence"] > 0.7].groupby(
+            by="cluster_id")["confidence"].count().sum()
+        cluster_with_max_duplicates = model_output.loc[model_output["confidence"] > 0.7].groupby(
+            by="cluster_id")["confidence"].count().idxmax()
         result = {
             "Total records": total_records,
             "Number of duplicate sets": number_of_clusters,
             "Duplicates": duplicates,
             "/% duplicates": duplicates / total_records,
-            "Cluster with maximum duplicates" : cluster_with_max_duplicates
+            "Cluster with maximum duplicates": cluster_with_max_duplicates
         }
         return result
 
@@ -76,9 +81,11 @@ class Deduplication:
         current_dir = os.path.dirname(os.path.realpath(__file__))
         target_dir = os.path.sep.join(current_dir.split(os.path.sep)[:-2])
         if os.path.isfile(target_dir + "/tiger/benchmark/dedupe_dataframe_learned_settings"):
-            os.remove(target_dir + "/tiger/benchmark/dedupe_dataframe_learned_settings")
+            os.remove(
+                target_dir + "/tiger/benchmark/dedupe_dataframe_learned_settings")
         if os.path.isfile(target_dir + "/tiger/benchmark/dedupe_dataframe_training.json"):
-            os.remove(target_dir + "/tiger/benchmark/dedupe_dataframe_training.json")
+            os.remove(
+                target_dir + "/tiger/benchmark/dedupe_dataframe_training.json")
         print("removed")
         if not df:
             df = self.get_data(self.raw_entity_table_name)
@@ -94,6 +101,7 @@ class Deduplication:
         if not os.path.isfile(target_dir + "/tiger/benchmark/dedupe_dataframe_training.json"):
             print("Cannot find dedupe_dataframe_training.json file")
         return self._run_model(df, col_names)
+
 
 if __name__ == "__main__":
     dedupe = Deduplication()
